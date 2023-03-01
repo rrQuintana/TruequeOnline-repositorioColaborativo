87547d482d23761155cd80fd374e35ab326307e3
rrQuintana <113273490+rrQuintana@users.noreply.github.com>
2023-03-01 16:33:28 -0600

    Comentario

--- Front-end/src/AuthContext.js
+++ Front-end/src/AuthContext.js
@@ -17,6 +17,8 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+//isUser es la información que proporciona la autenticación de firebase la cual utilizamos para obtener los datos de correo y en un futuro la foto de perfil
+//UserData es la información que se obtiene de la base de datos de mongo la cual utilizamos para obtener los datos de nombre, apellido, etc.
 
 //Este contexto se usa para saber de forma global si hay un usuario autenticado y para establecer si un usuario está autenticado. 
 //También se usa para obtener el objeto usuario y para establecer el objeto usuario.
