131a8ac9d6d2b9b4c1a4a1d5173232dbb96c2e28
rrQuintana <113273490+rrQuintana@users.noreply.github.com>
2023-03-01 17:18:26 -0600

    LiveShare

--- Front-end/src/components/Profile/Profile.js
+++ Front-end/src/components/Profile/Profile.js
@@ -1,17 +1,29 @@
-import React, { useState,useContext, useEffect } from "react";
-import { Container, Row, Col, Image, Button, Card } from "react-bootstrap";
+import React, { useState, useContext, useEffect } from "react";
+
+import { Container, Row, Col, Image,  Card } from "react-bootstrap";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import Modal from '@mui/material/Modal';
 import { NavBar } from "../LandingPage/NavBar";
 import { AuthContext } from "../../AuthContext";
 import { Link } from "react-router-dom";
-import DefaulUserIcon from "./default-user.png";
+import DefaultUserIcon from "./default-user.png";
 import "./Profile.css";
+import { db } from "../firebase.config";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import axi from "axios";
 
 function Profile() {
   const { isAuthenticated } = useContext(AuthContext); //Saber si el usuario está autenticado
   const { isUser } = useContext(AuthContext); //Obtener los datos del usuario
-
+  
   const [datosUsuaro, setDatosUsuario] = useState([]); //Manejadores de estado para datos de usuario
+  const [NombreUsuario, setNombreUsuario] = useState(""); //Manejadores de estado para Nombre de usuario
+  const [NoUsuario, setNoUsuario] = useState(""); //Manejadores de estado para Nombre de usuario
+  const [Telefono, setTelefono] = useState(""); //Manejadores de estado para Telefono
+  const [Correo, setCorreo] = useState(""); //Manejadores de estado para Correo
+  const [Contacto, setContacto] = useState(""); //Manejadores de estado para Contacto
 
   //Función para cargar los datos del usuariop
   useEffect(() => {
@@ -21,11 +33,25 @@ function Profile() {
       const res = await axi.get(`http://localhost:4000/api/usuarios/${campoBuscado}`)
       const InfoUsuario = res.data
       console.log(InfoUsuario)
-      setNombreUsuario(InfoUsuario.nombre);
-      setNoUsuario(InfoUsuario._id);
-      setTelefono(InfoUsuario.telefono);
-      setCorreo(InfoUsuario.correo);
-      setContacto(InfoUsuario.contacto);
+      setDatosUsuario(InfoUsuario)
+    async function CargarDatos() {
+      const campoBuscado = isUser.uid; //Id del usuario
+
+      //Hacer el querry de sus datos
+      const q = query(
+        collection(db, "usuarios"),
+        where("Usuario", "==", campoBuscado)
+      );
+
+      //Extraer los datos y almacenarlos en variables de estado
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setNombreUsuario(doc.data().Nombre);
+        setNoUsuario(doc.data().Usuario);
+        setTelefono(doc.data().Telefono);
+        setCorreo(doc.data().Correo);
+        setContacto(doc.data().Contacto);
+      });
     }
     CargarDatos();
   }, []);
@@ -49,6 +75,9 @@ function Profile() {
                   src={
                     datosUsuaro.foto
                       ? datosUsuaro.foto
+                      : DefaultUserIcon
+                    isUser.proactiveRefresh.user.photoURL
+                      ? isUser.proactiveRefresh.user.photoURL
                       : DefaulUserIcon
                   }
                   alt="profile pic"
@@ -60,13 +89,21 @@ function Profile() {
                 md={6}
                 className="d-flex row justify-content-center align-items-center"
               >
+                <p>{datosUsuaro.calificacion}</p>
+                <h1>{datosUsuaro.nombre + datosUsuaro.apellido}</h1>
+                <h3 className="mb-5">{datosUsuaro.email}</h3>
+                <p>Teléfono: {datosUsuaro.telefono}</p>
+                <p>Ubicación: {datosUsuaro.direccion}</p>
+                <p>Contacto: {datosUsuaro.contacto}</p>
+                <Button variant="primary">
+                  <Link className="text-white" to="/newProduct">Subir un producto</Link>
                 <h1>{NombreUsuario}</h1>
                 <h3 className="mb-5">{Correo}</h3>
                 <p>Teléfono: {Telefono}</p>
                 <p>Contacto: {Contacto}</p>
                 <p>Id usuario: {NoUsuario}</p>
-                <Button variant="primary">
-                  <Link className="text-white" to="/newProduct">Subir un producto</Link>
+                <Button variant="contained">
+                  <li className="fas fa-edit"></li> Editar
                 </Button>
               </Col>
             </Row>
